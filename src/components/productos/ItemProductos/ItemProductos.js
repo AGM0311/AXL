6d23642dfc044f23efdc30ddf8d917b7c6dc4b1a
AXL <axlgamas@outlook.com>
2025-02-04 21:35:27 -0600

    nuevos

--- src/components/productos/ItemProductos/ItemProductos.js
+++ src/components/productos/ItemProductos/ItemProductos.js
@@ -1,9 +1,24 @@
 import React from 'react'
+//import {Datos} from '../../../utils/bd';
 
-export function ItemProductos() {
+
+export function ItemProductos({producto}) {
   return (
     <div>
-      ItemProductos
+         <div className="card" style={{ width: '18rem' }}>
+  <img src={producto.imagen} class="card-img-top" alt="{producto.nombre}"  />
+  <div className="card-body">
+    <h5 className="card-title">{producto.nombre}</h5>
+    
+  </div>
+  <ul class="list-group list-group-flush">
+    <li className="list-group-item">Precio: ${producto.precio}</li>
+    <li className="list-group-item">Cantidad disponible: {producto.cantidad}</li>
+    <li className="list-group-item">{producto.contenido || producto.unidad}</li>
+  </ul>
+  <div class="card-body">
+  </div>
+</div>
     </div>
   )
 }
---
+++ src/utils/bd.js
@@ -0,0 +1,23 @@
+export const Datos=[
+    {
+        nombre:"coca cola",
+        precio:18,
+        cantidad:20,
+        contenido:'600ml',
+        imagen: "/images/coca.jpg",
+    },
+    {
+        nombre:"chokis",
+        precio:9,
+        cantidad:15,
+        unidad:'350gr',
+        imagen: "/images/chokis.jpg",
+    },
+    {
+        nombre:"chetos",
+        precio:14,
+        cantidad:25,
+        unidad:'400gr',
+        imagen: "/images/chetos.jpg",
+    }
+]
